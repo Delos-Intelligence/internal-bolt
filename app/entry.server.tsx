4b5d9b336d7b993bc75c0c853c36a7a425d4734d
Maximilien de Dinechin <m.dinechin@protonmail.com>
2024-11-25 15:16:44 +0100

    Server config

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,12 +1,11 @@
 import type { AppLoadContext, EntryContext } from '@remix-run/cloudflare';
 import { RemixServer } from '@remix-run/react';
 import { isbot } from 'isbot';
+import { renderToReadableStream } from 'react-dom/server';
 import { renderHeadToString } from 'remix-island';
 import { Head } from './root';
 import { themeStore } from '~/lib/stores/theme';
 import { initializeModelList } from '~/utils/constants';
-
-const { renderToReadableStream } = await import('react-dom/server');
 
 export default async function handleRequest(
   request: Request,
